fabcec7b69b6a01b0ad78deb0c7c91597a9d25d6
Anthony <anthonyjbmolina@gmail.com>
2025-04-08 02:57:41 -0400

    finalize Sanity Studio setup – added visual editing + full integration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,15 +4,25 @@ import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+import { loadEnv } from "vite";
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  ""
+);
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     sanity({
-      projectId: "aahhyxfc",
-      dataset: "production",
+      projectId: PUBLIC_SANITY_PROJECT_ID,
+      dataset: PUBLIC_SANITY_DATASET,
       useCdn: false, // See note on using the CDN
       apiVersion: "2025-01-28", // insert the current date to access the latest version of the API
-      studioBasePath: "/studio", // Optional: defaults to /studio
+      studioBasePath: "/admin",
+      stega: {
+        studioUrl: "/admin",
+      },
     }),
     react(),
   ],
